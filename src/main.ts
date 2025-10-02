ee9d5cb10c85e16468aa499a921597bfeccbbb9b
Tarek Saad <tareksaad.ds@gmail.com>
2025-10-02 13:02:26 +0300

    server

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const port = process.env.PORT ?? 8080;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 8080);
+  await app.listen(port);
 }
 bootstrap();
